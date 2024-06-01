e8c275ef8b731040c395c97cbb5fb0ab49f40638
Paul Abel <abelpaul198@gmail.com>
2024-06-01 14:33:03 +0200

    Check if category exist when creating product

--- services/product/src/main/java/de/shop/product/controller/ProductController.java
+++ services/product/src/main/java/de/shop/product/controller/ProductController.java
@@ -1,5 +1,7 @@
 package de.shop.product.controller;
 
+import de.shop.product.clients.CategoryClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,9 @@ import java.util.Optional;
 public class ProductController {
 
     private final ProductService service;
+
+    @Autowired
+    private CategoryClient categoryClient;
 
     public ProductController(ProductService service) {
         this.service = service;
@@ -51,6 +56,11 @@ public class ProductController {
         if (product.getCategoryId() == null || product.getName() == null) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Category ID and name cannot be null");
         }
+        var category = categoryClient.getCategory(product.getCategoryId());
+        if (category == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Category ID not known");
+        }
+
         this.service.add(product);
         return new ResponseEntity<>(this.service.findById(product.getId()), HttpStatus.CREATED);
     }
